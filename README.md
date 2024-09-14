51c6624dd636946b324fed825b14cd34324dbf12
banderGlitch <nernaykumar98@gmail.com>
2024-09-14 23:19:31 +0530

    firstcomit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React + Vite
+# Connect wallet template for wallet integration
+
+npm create vite@latest
+
+npm install @web3modal/wagmi wagmi viem @tanstack/react-query
+
+wallentconnect
+https://cloud.walletconnect.com/
+project id :- 2c4250b0ed1c4c85027974613fc83eaf
+Varanasi@941
+
+# connect chain , arbitrum, mainnet , hedera, hederaTestnet, polygon , polygonAmoy, rootstock , rootstockTestnet
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { WagmiProvider } from 'wagmi'
+import { arbitrum, mainnet , hedera, hederaTestnet, polygon , polygonAmoy, rootstock , rootstockTestnet } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import ConnectButton from './ConnectButton'
+
+
+// 1. Get projectId
+const projectId = '2c4250b0ed1c4c85027974613fc83eaf'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'deApp',
+  description: 'deApp for testing',
+  url: 'http://localhost:5173', // Update this to match your dev server port
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum, hedera, hederaTestnet, polygon , polygonAmoy, rootstock , rootstockTestnet ]
+const config = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig: config, projectId, chains })
+
+// 4. Create query client
+const queryClient = new QueryClient()
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectButton />
+      </QueryClientProvider>
+    </WagmiProvider>
+     </>
+  )
+}
+
+export default App

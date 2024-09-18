d6682a52ab2a71337128b86b9226e648a9b35473
banderGlitch <nernaykumar98@gmail.com>
2024-09-18 16:38:58 +0530

    2ndcommit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import { Box, Heading, Text } from "@metamask/snaps-sdk/jsx";
 
-export const walletAddress = await snap.request({
-  method: "snap_dialog",
-  params: {
-    type: "prompt",
-    content: (
-      <Box>
-        <Heading>What is the wallet address?</Heading>
-        <Text>Please enter the wallet address to be monitored</Text>
-      </Box>
-    ),
-    placeholder: "0x123...",
-  },
-});
+export const onRpcRequest = async ({ origin, request }) => {
+  switch (request.method) {
+    case 'getWalletAddress':
+      return snap.request({
+        method: "snap_dialog",
+        params: {
+          type: "prompt",
+          content: (
+            <Box>
+              <Heading>What is the wallet address?</Heading>
+              <Text>Please enter the wallet address to be monitored</Text>
+            </Box>
+          ),
+          placeholder: "0x123...",
+        },
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+};
+
+// import { Box, Heading, Text } from "@metamask/snaps-sdk/jsx";
+
+// export const walletAddress = await snap.request({
+//   method: "snap_dialog",
+//   params: {
+//     type: "prompt",
+//     content: (
+//       <Box>
+//         <Heading>What is the wallet address?</Heading>
+//         <Text>Please enter the wallet address to be monitored</Text>
+//       </Box>
+//     ),
+//     placeholder: "0x123...",
+//   },
+// });
 
 // walletAddress will be a string containing the address entered by the user. 
 // import { OnRpcRequestHandler } from '@metamask/snaps-types';
